7ff3805509e12af86a82386f0b1de3e0f56520a9
Amr A. Abdelmoez <amradel88@live.com>
2018-09-15 15:48:28 +0200

    feat: Update DOM elements

--- app.js
+++ app.js
@@ -4,6 +4,8 @@
 
     const next = document.getElementById('new-quote');
     const prev = document.getElementById('prev-quote');
+    const quoteText = document.getElementById('text');
+    const quoteAuthor = document.getElementById('author');
 
     const quotes = [];
 
@@ -11,7 +13,17 @@
     prev.addEventListener('click', () => getQuote('prev'))
 
     const getQuote = (direction) => {
-        console.log(direction);
+        let quote = 'Graphic design is the design of highly disposable items… It all winds up in the garbage';
+        let author = 'Karrie Jacobs';
+
+        return updateDOM(quote, author)
     }
+
+    const updateDOM = (quote, author) => {
+        quoteText.innerText = quote;
+        quoteAuthor.innerText = author;
+    }
+
+    getQuote('next');
 
 })()
